4b9587f4c76ae3cdd6476f2e6038283fe5097fa2
Harun Sevinc <Harun.Sevinc@ext.markant.com>
2023-01-18 16:28:53 +0100

    added rabbitmq connection to send to queue

--- RabbitMQ/src/main/java/com/harunsevinc/events/RabbitMQEventListenerProvider.java
+++ RabbitMQ/src/main/java/com/harunsevinc/events/RabbitMQEventListenerProvider.java
@@ -1,5 +1,8 @@
 package com.harunsevinc.events;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import org.jboss.logging.Logger;
 import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
@@ -7,10 +10,16 @@ import org.keycloak.events.EventType;
 import org.keycloak.events.admin.AdminEvent;
 import org.keycloak.models.KeycloakSession;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class RabbitMQEventListenerProvider implements EventListenerProvider {
 
     private static final Logger log = Logger.getLogger(RabbitMQEventListenerProvider.class);
     private final KeycloakSession keycloakSession;
+
+    private final static String QUEUE_NAME = "YourQueueName";
+
 
     public RabbitMQEventListenerProvider(KeycloakSession keycloakSession) {
         this.keycloakSession = keycloakSession;
@@ -18,11 +27,23 @@ public class RabbitMQEventListenerProvider implements EventListenerProvider {
 
     @Override
     public void onEvent(Event event) {
-        if(event.getType() == EventType.LOGIN){
-            //Do something here
-            log.info("User has logged in!");
-        }
+        //Check for event or add as Many events as you like
+        if(event.getType() == EventType.REGISTER){
 
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+
+            try (Connection connection = factory.newConnection();
+                Channel channel = connection.createChannel()) {
+                channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+                // Your message could also be an object which need to be deserialized
+                String message = "Some Message";
+                channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+                System.out.println(" [x] Sent '" + message + "'");
+            } catch (IOException | TimeoutException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @Override
